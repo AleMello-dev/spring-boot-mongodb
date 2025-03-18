ed9cd07da10d42b83ef7b43542a26e26f5650618
AleMello-dev <alemello188@outlook.com>
2025-03-18 20:44:05 -0300

    Consulta com varios criterios

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -17,15 +17,17 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 	
-	public Post findById(String id) { 
-		 Optional<Post> obj = repo.findById(id); 
-		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
-		}
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
 	
-	
-	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
